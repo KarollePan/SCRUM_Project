d62b28b05979288e301c710422f8fb04f69b5eb9
KarollePan <k.pangonyte@gmail.com>
2020-02-11 09:31:47 +0000

    runtime

--- scrum_template/env/bin/activate.fish
+++ scrum_template/env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Karolle/git/scrum_template/env"
+set -gx VIRTUAL_ENV "/Users/Karolle/git/scrum_template 2/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
