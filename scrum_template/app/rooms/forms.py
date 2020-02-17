cc3269b77b33284fb2ebf054c2297aa1758a06d8
KarollePan <k.pangonyte@gmail.com>
2020-02-17 02:46:46 +0000

    Created AddRoomForm and EditRoomForm

---
+++ scrum_template/app/rooms/forms.py
@@ -0,0 +1,25 @@
+
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, BooleanField, SubmitField 
+from wtforms import TextAreaField, FileField, DecimalField, IntegerField, SelectField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, NumberRange
+
+class AddRoomForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(),Length(min=4, max=30)])
+    description = TextAreaField('Description', validators=[DataRequired(), Length(min=50, max=5000)])
+    image = FileField('Add image', validators=[FileAllowed(['jpg', 'png'])])
+    price = DecimalField('Price', validators=[DataRequired()])    submit = SubmitField('Add room')
+
+class EditRoomForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(),Length(min=4, max=100)])
+    description = TextAreaField('Description', validators=[DataRequired(), Length(min=50, max=5000)])
+    image = FileField(
+        'Update room image - <span style="color:red;">only replace if old or incorrect</span>', 
+        validators=[
+            FileAllowed(['jpg', 'png'], 'Images only with extension .jpg or .png')
+        ]
+    )
+    price = DecimalField('Price', validators=[DataRequired()])
+    update = SubmitField('Update')
+    cancel = SubmitField('Cancel')
