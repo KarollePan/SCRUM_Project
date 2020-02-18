9a9c7de8efb807b03ae53f2c37582931f29666de
KarollePan <k.pangonyte@gmail.com>
2020-02-18 22:40:36 +0000

    Added Edit forms for Wed and Tour bookings

--- scrum_template/app/bookings/forms.py
+++ scrum_template/app/bookings/forms.py
@@ -19,6 +19,21 @@ class AddWeddingBookingForm(FlaskForm):
     incl_photography = BooleanField('Photography')
     submit = SubmitField('Submit')
 
+class EditWeddingBookingForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    telno = StringField('Telephone Number',validators=[DataRequired()])
+    date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
+    incl_catering = BooleanField('Catering')
+    incl_flowers = BooleanField('Flowers')
+    incl_carhire = BooleanField('Car Hire')
+    incl_visual_and_audio = BooleanField('Visual & Audio System')
+    incl_photography = BooleanField('Photography')
+    update = SubmitField('Update')
+    cancel = SubmitField('Cancel')
+    delete = SubmitField('Delete')
+
 class AddTourBookingForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
@@ -27,3 +42,14 @@ class AddTourBookingForm(FlaskForm):
     date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
     time_id = SelectField('Time', coerce=int)
     submit = SubmitField('Submit')
+
+class EditTourBookingForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    telno = StringField('Telephone Number',validators=[DataRequired()])
+    date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
+    time_id = SelectField('Time', coerce=int)
+    update = SubmitField('Update')
+    cancel = SubmitField('Cancel')
+    delete = SubmitField('Delete')
